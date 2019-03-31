5451cf81346a9f823a82a247d0d9938b7b1fdabe
rudovs <extrazeto@gmail.com>
2019-04-01 00:46:11 +0300

    update

--- src/lesson8/std/Demo.java
+++ src/lesson8/std/Demo.java
@@ -1,18 +1,24 @@
 package lesson8.std;
 
-import java.util.Date;
-
 public class Demo {
     public static void main(String[] args) {
-        createHighestParent();
+        Demo.createHighestParent();
+        Demo.createLowestChild();
 
     }
-    static void createHighestParent(){
-        Student[] student = new Student[1];
-        CollegeStudent[] collegeStudents = new CollegeStudent[1];
+    static SpecialStudent[] createHighestParent(){
+//        Student[] student = new Student[1];
+//        CollegeStudent[] collegeStudents = new CollegeStudent[1];
         SpecialStudent[] specialStudents = new SpecialStudent[1];
-        student[1] = new Student("Ivan", "Ivanov", 472,null,23);
-        collegeStudents[1] = new CollegeStudent("CollegeStudent", "StdLastname", 233, null, 56);
+//        student[1] = new Student("Ivan", "Ivanov", 472,null,23);
+//        collegeStudents[1] = new CollegeStudent("CollegeStudent", "StdLastname", 233, null, 56);
         specialStudents[1] = new SpecialStudent("SpecialName", "SpecialLastname", 295, null, 24, 23423423,"123@mail.com");
+        return specialStudents;
+    }
+
+    static Student[] createLowestChild() {
+        Student[] student = new Student[1];
+        student[1] = new Student("Ivan", "Ivanov", 33, null, 21);
+        return student;
     }
 }

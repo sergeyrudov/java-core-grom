d93d315ff4e34bf62b9659773f3a02bede254230
sergeyrudov <extrazeto@gmail.com>
2019-12-21 15:58:49 +0200

    lesson1&9 update

--- src/lesson9/utils/DemoTwo.java
+++ src/lesson9/utils/DemoTwo.java
@@ -1,6 +1,6 @@
 package lesson9.utils;
 
-public class Demo {
+  public class DemoTwo {
     public static void main(String[] args) {
         Checker checker = new Checker();
         System.out.println(checker.companyNamesValidatedCount);

b053c4e697da00b8e7ce0cfa7fa46a758514e78f
sergeyrudov <extrazeto@gmail.com>
2020-01-11 19:33:56 +0200

    lesson10

--- src/lesson1/FinancialCompany.java
+++ src/lesson1/FinancialCompany.java
@@ -2,11 +2,11 @@ package lesson1;
 
 import lesson9.Company;
 
-public class FinancialCompany extends Company {
-    public static void main(String[] args) {
+//public class FinancialCompany extends Company {
+//    public static void main(String[] args) {
 
-    }
-    void demonstrateProtectedModifier(){
-        System.out.println(this.code);
-    }
-}
+ //   }
+ //   void demonstrateProtectedModifier(){
+ //       System.out.println(this.code);
+ //   }
+//}
--- src/lesson1/FirstProgram.java
+++ src/lesson1/FirstProgram.java
@@ -4,7 +4,7 @@ import lesson9.utils.Checker;
 
 public class FirstProgram {
     public static void main(String[] args) {
-        System.out.println("My firsh Java program..");
+        System.out.println("My first Java program..");
 
         System.out.println("Done");
         Checker checker = new Checker();

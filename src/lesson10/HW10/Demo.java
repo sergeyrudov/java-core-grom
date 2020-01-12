1d0b1f8c55c364621bdc0f40ba68e234723e08e8
sergeyrudov <extrazeto@gmail.com>
2020-01-12 18:44:38 +0200

    HW10

--- src/lesson10/HW10/Demo.java
+++ src/lesson10/HW10/Demo.java
@@ -6,11 +6,11 @@ public class Demo {
     public static void main(String[] args) {
         Customer customer = new Customer("Кирил", "Киев", "Мужской");
 
-        ElectronicsOrder e1 = new ElectronicsOrder("Холодильник", new Date(), "Киев", "Одесса", 4876, customer, 12);
-        ElectronicsOrder e2 = new ElectronicsOrder("Чайник", new Date(), "Киев", "Львов", 800, customer, 3);
+        ElectronicsOrder e1 = new ElectronicsOrder("TV", new Date(), "Киев", "Одесса", 12000, customer, 24);
+        ElectronicsOrder e2 = new ElectronicsOrder("AudioSet", new Date(), "Киев", "Львов", 500, customer, 6);
 
-        FurnitureOrder f1 = new FurnitureOrder("Ручка", new Date(), "Киев", "Харьков", 250, customer, "12432");
-        FurnitureOrder f2 = new FurnitureOrder("Гвоздь", new Date(), "Киев", "Винница", 24, customer, "13532");
+        FurnitureOrder f1 = new FurnitureOrder("Dverca", new Date(), "Киев", "Харьков", 100, customer, "10000");
+        FurnitureOrder f2 = new FurnitureOrder("Vintik", new Date(), "Киев", "Винница", 10, customer, "14666");
         //electronic 1 order
         e1.validateOrder();
         e1.calculatePrice();

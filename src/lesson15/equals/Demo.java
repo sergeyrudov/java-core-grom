7e9771831dcd35929332f8f9e058c5de5b144846
sergeyrudov <extrazeto@gmail.com>
2020-02-23 01:36:17 +0200

    lesson15

---
+++ src/lesson15/equals/Demo.java
@@ -0,0 +1,8 @@
+package lesson15.equals;
+
+public class Demo {
+    public static void main(String[] args) {
+        File file1 = new File(111, "home/Documents/test", "txt");
+        File file2 = new File(111, "home/Documents/image", "jpg");
+    }
+}

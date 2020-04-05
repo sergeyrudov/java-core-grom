e06f82362cada32f2c7587bffdb8307cbdb36cc9
sergeyrudov <255400warr>
2020-04-05 12:02:33 +0300

    lesson17

---
+++ src/lesson17/Solution.java
@@ -0,0 +1,118 @@
+package lesson17;
+
+public class Solution {
+    public static void main(String[] args) {
+        String test = "helloworld, hello, world, test";
+        System.out.println(countWords(test));
+        System.out.println(maxWord(test));
+        System.out.println(minWord(test));
+        System.out.println(mostCountedWord(test));
+    }
+
+    public static int countWords(String input) {
+        int j, n = 0;
+        for (int i = 0; i < input.length(); i = j) {
+            while ((i < input.length()) && !Character.isLetter(input.charAt(i)))
+                i++;
+
+            j = i + 1;
+            while ((j < input.length()) && Character.isLetter(input.charAt(j)))
+                j++;
+
+            if (i != input.length())
+                n++;
+        }
+        return n;
+    }
+
+    public static String mostCountedWord(String input) {
+        if (input.isEmpty()) {
+            return null;
+        }
+
+        String[] words = input.split(" ");
+
+        int maxCount = 0;
+        String mostFrequent = null;
+        for (String word : words) {
+            int count = 0;
+            for (String item : words) {
+                if (word.equals(item) && word.length() > 0 && Character.isLetter(item.charAt(0))) {
+                    count++;
+                }
+
+                if (count > maxCount) {
+                    maxCount = count;
+                    mostFrequent = word;
+                }
+            }
+        }
+        return mostFrequent;
+    }
+
+
+    public static String maxWord(String input) {
+        String[] words = input.split(" ");
+
+        if (words.length == 0) {
+            return null;
+        }
+
+        String max = null;
+
+        for (String word : words) {
+            if (word.length() > 0 && checkLetter(word)) {
+                max = word;
+                break;
+            }
+        }
+
+        if (max == null){
+            return null;
+        }
+
+        for (String word : words){
+            if (word.length() > max.length() && word.length() > 0 && checkLetter(word))
+                max = word;
+        }
+
+        return max;
+    }
+
+    public static String minWord(String input) {
+        String[] words = input.split(" ");
+        if (words.length == 0) {
+            return null;
+        }
+
+        String min = null;
+        for (String word : words) {
+            if (word.length() > 0 && checkLetter(word)) {
+                min = word;
+                break;
+            }
+        }
+
+        if (min == null){
+            return null;
+        }
+
+        for (String word : words){
+            if (word.length() < min.length() && word.length() > 0 && checkLetter(word))
+                min = word;
+        }
+
+        return min;
+    }
+
+    public static boolean checkLetter(String name) {
+        char[] chars = name.toCharArray();
+
+        for (char c : chars) {
+            if (!Character.isLetter(c)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
